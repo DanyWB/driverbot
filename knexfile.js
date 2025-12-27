0fa2028e0fafece208a11a131648741ed590d162
DanyWB <dany6191@mail.ru>
2025-12-27 16:34:28 +0300

    fix connect db

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 // knexfile.js
 const DEV_CONNECTION = {
-  host: process.env.PG_HOST || "localhost",
+  host: process.env.PG_HOST || "127.0.0.1",
   port: Number(process.env.PG_PORT || 5432),
-  user: process.env.PG_USER || "postgres",
-  password: process.env.PG_PASSWORD || "3010",
-  database: process.env.PG_DATABASE || "bike_rent",
+  user: process.env.PG_USER || "driverbot_user",
+  password: process.env.PG_PASSWORD || "1337",
+  database: process.env.PG_DATABASE || "driverbot",
 };
 
 const PROD_CONNECTION = process.env.DATABASE_URL
